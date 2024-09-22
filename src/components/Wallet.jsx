b436a8978596e79e01f9c77bd9cc22c45a07d61e
bayko04 <superkolo2017@gmail.com>
2024-09-22 17:11:45 +0600

    changed

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -10,24 +10,24 @@ export const Wallet = () => {
   const [balance, setBalance] = useState(null);
   const [userFriendlyAddress, rawAddress] = useAddress();
 
-  // useEffect(() => {
-  //   const fetchBalance = async () => {
-  //     try {
-  //       const adress = await fetch(
-  //         `https://toncenter.com/api/v2/getAddressBalance?address=${userFriendlyAddress}`
-  //       );
-  //       const balance = await adress.json();
-  //       setBalance(balance);
-  //     } catch (error) {
-  //       console.error("Ошибка получения баланса:", error);
-  //       setBalance(0);
-  //     }
-  //   };
+  useEffect(() => {
+    if (userFriendlyAddress) {
+      const fetchBalance = async () => {
+        try {
+          const adress = await fetch(
+            `https://toncenter.com/api/v2/getAddressBalance?address=${userFriendlyAddress}`
+          );
+          const balance = await adress.json();
+          setBalance(balance);
+        } catch (error) {
+          console.error("Ошибка получения баланса:", error);
+          setBalance(0);
+        }
+      };
 
-  //   fetchBalance();
-  // }, [userFriendlyAddress]);
-
-  console.log(userFriendlyAddress);
+      fetchBalance();
+    }
+  }, [userFriendlyAddress]);
 
   return (
     <>
